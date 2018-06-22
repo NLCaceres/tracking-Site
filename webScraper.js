b923844669b718268cb834d3cac25211de6940a9
NLCaceres <NLCaceres21@Gmail.com>
2018-06-22 15:18:49 -0700

    Adding to see if works

--- webScraper.js
+++ webScraper.js
@@ -1,8 +1,19 @@
-document.getElementById('searchBar').onclick = searchFunction()
-{
-    location.href = document.getElementById('searchBar').value;
-};
+$(document).ready(function() {
 
-searchFunction() {
+    if ($("#search-bar").text().length > 0) {
+        var searchTerm = $("#search-bar").text();
+        var regexPatt = new RegExp(searchTerm, "gi");
+        var columnNames = $(".name-column")
+        $("#search-button").click(findKeyWord(columnNames, regexPatt));
+    }
     
+});
+
+function findKeyWord(names, patt) {
+    for (var name in names) {
+        if (name.match(patt)) {
+            console.log("Found a match!");
+            console.log("It matched with: " + name);
+        }
+    }
 }
