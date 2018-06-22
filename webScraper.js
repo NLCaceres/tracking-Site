bd31d3d4aa5c7387eb25183bba861cefaad1f489
NLCaceres <NLCaceres21@Gmail.com>
2018-06-22 15:41:11 -0700

    Beginning search button work

--- webScraper.js
+++ webScraper.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
 
-    if ($("#search-bar").text().length > 0) {
-        var searchTerm = $("#search-bar").text();
-        var regexPatt = new RegExp(searchTerm, "gi");
-        var columnNames = $(".name-column")
-        $("#search-button").click(findKeyWord(columnNames, regexPatt));
-    }
+
+    $("#search-button").click(function () {
+        console.log("Got a click from the submit button");
+        var searchTerm = "";
+        if ($("#search-bar").text().length > 0) {
+            searchTerm = $("#search-bar").text();
+            var regexPatt = new RegExp(searchTerm, "gi");
+            var columnNames = $(".name-column");
+            findKeyWord(columnNames, regexPatt);
+        }
+    });
+    
     
 });
 
 function findKeyWord(names, patt) {
+    console.log("Calling on this function");
     for (var name in names) {
         if (name.match(patt)) {
             console.log("Found a match!");
